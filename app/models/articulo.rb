49a8b767246a1bf07a62f99ea87a54b1d2583474
Kike Medina <kikemedca@gmail.com>
2017-03-07 12:32:08 +0100

    añadir artículos

--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -1,3 +1,7 @@
-class Articulo < ApplicationRecord
+class Articulo < ActiveRecord::Base
   belongs_to :autor
+  validates :titulo, presence: {:message => "no debe estar vacío"}
+  validates :contenido, presence: {:message => "no debe estar vacío"}, length: {minimum: 50, :message => "es demasiado corto, mínimo 50 caracteres"}
 end
+
+#ApplicationRecord
